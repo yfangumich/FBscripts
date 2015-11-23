b727b730a0d4ac7c71e25cc6fbb567f7415a4230
Yu Fang <yfang@umich.edu>
2015-11-23 17:16:16 -0500

    Function to extract start and end date

--- Data_Explore.R
+++ Data_Explore.R
@@ -1,1 +1,14 @@
+setClass(Class="Subject",representation(id="factor",
+                                        start="factor",
+                                        end="factor"))
+DateRange=function(y,x){
+  dates=which(y$Id %in% x)
+  start=y$SleepDay[dates[1]]
+  end=y$SleepDay[dates[length(dates)]]
+  return(new("Subject",id=x,start=start, end=end))
+}
+# day sleep
 Sleep2014=read.csv('work/Fitbit/2014_Cohort_all//sleepDay_merged.csv')
+SubjIDs=unique(Sleep2014$Id)
+t=DateRange(Sleep2014,SubjIDs[1])
+
