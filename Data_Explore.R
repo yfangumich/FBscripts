1edccef60635fb75ee423a56aee65f65eb1aa431
Yu Fang <yfang@umich.edu>
2015-11-25 09:06:42 -0500

    Extract date range for each subject

--- Data_Explore.R
+++ Data_Explore.R
@@ -1,14 +1,21 @@
-setClass(Class="Subject",representation(id="factor",
-                                        start="factor",
-                                        end="factor"))
 DateRange=function(y,x){
   dates=which(y$Id %in% x)
   start=y$SleepDay[dates[1]]
   end=y$SleepDay[dates[length(dates)]]
-  return(new("Subject",id=x,start=start, end=end))
+  out=data.frame(x,start,end)
+  colnames(out)=c("id","start","end")
+  return(out)
 }
 # day sleep
 Sleep2014=read.csv('work/Fitbit/2014_Cohort_all//sleepDay_merged.csv')
 SubjIDs=unique(Sleep2014$Id)
-t=DateRange(Sleep2014,SubjIDs[1])
-
+DR=data.frame(id=character(),
+                     start=character(),
+                     end=character())
+for (i in 1:length(SubjIDs)){
+t=DateRange(Sleep2014,SubjIDs[i])
+t$id=as.character(t$id)
+t$start=as.character(t$start)
+t$end=as.character(t$end)
+DR=rbind(DR,t)
+}
